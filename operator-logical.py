2132e94ec97e8e89e112f507b2ecf1b04baa9006
Abdullah Alam Ansari <abdullahalamofc57@gmail.com>
2026-01-25 19:16:33 +0500

    operator done

--- operator-logical.py
+++ operator-logical.py
@@ -3,3 +3,35 @@ b = False
 print(a and b)
 print(a or b)
 print(not a)
+
+a = 10
+b = 20
+c = a
+
+print(a is not b) # identity operators  
+print(a is c)
+
+x = 24
+y = 20
+list = [10, 20, 30, 40, 50]
+
+if (x not in list):
+    print("x is NOT present in given list")
+else:
+    print("x is present in given list")
+
+if (y in list):
+    print("y is present in given list")
+else:
+    print("y is NOT present in given list")
+
+
+expr = 10 + 20 * 30
+print(expr)
+name = "Alex"
+age = 0
+
+if name == "Alex" or name == "John" and age >= 2:
+    print("Hello! Welcome.")
+else:
+    print("Good Bye!!")
