1756ee5fa80b63fc7178107fb37eae244f9d8328
Abdullah Alam Ansari <abdullahalamofc57@gmail.com>
2026-01-26 10:54:56 +0500

    Directory done

---
+++ stringslice.py
@@ -0,0 +1,6 @@
+name = input("Enter the name of the person: ")
+a = int(input("Enter the first index to be sliced: "))
+b = int(input("Enter the second index to be sliced: "))
+result = name[a:b]
+
+print(result)
