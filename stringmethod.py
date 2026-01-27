7af34f096bf90900cadc5908ed9370b5554c56fe
Abdullah Alam Ansari <abdullahalamofc57@gmail.com>
2026-01-27 23:48:33 +0500

    Method applied

---
+++ stringmethod.py
@@ -0,0 +1,9 @@
+text = "Hello,Bye!!!"
+
+print(text.upper())
+print(text.lower())
+print(text.title())
+print(text.split(','))
+print(text.endswith('!'))
+print(text.center(20,'-'))
+print(len(text))
---
+++ time.py
@@ -0,0 +1,6 @@
+import time
+curr = time.ctime(1627908313.717886)
+print("Current time:", curr)
+
+if(a > 4 | a<5):
+    
